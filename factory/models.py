2ef9b81e840846ad749fdc45ffffe9500bf0262e
saulshao <saulshao@yahoo.com>
2016-06-25 22:16:27 +0800

    change table name

--- factory/models.py
+++ factory/models.py
@@ -3,53 +3,53 @@ from django.db import models
 # Create your models here.
 class Nation(models.Model):
     code = models.CharField(max_length=5)
-    name = models.Charfield(max_length=200)
-    desc = models.Charfield(max_length=2000)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=2000)
 
     class Meta:
-        db_table = 'nation'
+        db_table = u'nation'
 
 class City(models.Model):
     nation_id = models.BigIntegerField
     code = models.CharField(max_length=20)
-    name = models.Charfield(max_length=200)
-    desc = models.Charfield(max_length=2000)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=2000)
 
     class Meta:
-        db_table = 'city'
+        db_table = u'city'
 
 class Company(models.Model):
     code = models.CharField(max_length=20)
-    name = models.Charfield(max_length=200)
-    desc = models.Charfield(max_length=2000)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=2000)
 
     class Meta:
-        db_table = 'company'
+        db_table = u'company'
 
 class Factory(models.Model):
     city_id = models.BigIntegerField
     company_id = models.BigIntegerField
     code = models.CharField(max_length=20)
-    name = models.Charfield(max_length=200)
-    desc = models.Charfield(max_length=2000)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=2000)
 
     class Meta:
-        db_table = 'factory'
+        db_table = u'factory'
 
 class Workshop(models.Model):
     factory_id = models.BigIntegerField
     code = models.CharField(max_length=20)
-    name = models.Charfield(max_length=200)
-    desc = models.Charfield(max_length=2000)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=2000)
 
     class Meta:
-        db_table = 'workshop'
+        db_table = u'workshop'
 
 class Line(models.Model):
     workshop_id = models.BigIntegerField
     code = models.CharField(max_length=20)
-    name = models.Charfield(max_length=200)
-    desc = models.Charfield(max_length=2000)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=2000)
 
     class Meta:
-        db_table = 'line'
+        db_table = u'line'
--- studymes/settings.py
+++ studymes/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'factory.apps.FactoryConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -85,6 +86,7 @@ DATABASES = {
         'PASSWORD': 'zaq12wsx',
         'HOST': '127.0.0.1',
         'PORT': '52000',
+        'SCHEMAS': 'public,core,config,material,interface', 
     },
     'other': {
         'ENGINE': 'django.db.backends.sqlite3',
