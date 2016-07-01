baeda64dc2378c5d2205d07f94cab496c353d517
saulshao <saulshao@yahoo.com>
2016-07-01 23:40:51 +0800

    add foreignkey

--- factory/admin.py
+++ factory/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(Nation)
+admin.site.register(City)
+admin.site.register(Company)
+admin.site.register(Factory)
+admin.site.register(Workshop)
+admin.site.register(Line)
--- factory/models.py
+++ factory/models.py
@@ -10,7 +10,7 @@ class Nation(models.Model):
         db_table = u'nation'
 
 class City(models.Model):
-    nation_id = models.BigIntegerField
+    models.ForeignKey(Nation, on_delete=models.CASCADE)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     desc = models.CharField(max_length=2000)
@@ -27,8 +27,8 @@ class Company(models.Model):
         db_table = u'company'
 
 class Factory(models.Model):
-    city_id = models.BigIntegerField
-    company_id = models.BigIntegerField
+    models.ForeignKey(City, on_delete=models.CASCADE)
+    models.ForeignKey(Company, on_delete=models.CASCADE)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     desc = models.CharField(max_length=2000)
@@ -37,7 +37,7 @@ class Factory(models.Model):
         db_table = u'factory'
 
 class Workshop(models.Model):
-    factory_id = models.BigIntegerField
+    models.ForeignKey(Factory, on_delete=models.CASCADE)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     desc = models.CharField(max_length=2000)
@@ -46,7 +46,7 @@ class Workshop(models.Model):
         db_table = u'workshop'
 
 class Line(models.Model):
-    workshop_id = models.BigIntegerField
+    models.ForeignKey(Workshop, on_delete=models.CASCADE)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
     desc = models.CharField(max_length=2000)
