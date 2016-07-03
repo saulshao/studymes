0cd2ed9d18ccf63a00fa74f643776d779119e101
saulshao <saulshao@yahoo.com>
2016-07-03 19:40:17 +0800

    ignore the migrations

--- factory/admin.py
+++ factory/admin.py
@@ -3,10 +3,71 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-admin.site.register(Nation)
-admin.site.register(City)
-admin.site.register(Company)
-admin.site.register(Factory)
-admin.site.register(Workshop)
-admin.site.register(Line)
-admin.site.register(Station)
+class ChoiceInlineForNation(admin.TabularInline ):
+    model = City
+    extra = 0
+
+class NationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc')
+    inlines = [ChoiceInlineForNation]
+
+class ChoiceInlineForCity(admin.TabularInline ):
+    model = Factory
+    extra = 0
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc', 'nation')
+    inlines = [ChoiceInlineForCity]
+
+class ChoiceInlineForCompany(admin.TabularInline ):
+    model = Department
+    extra = 0
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc')
+    inlines = [ChoiceInlineForCompany]
+
+class ChoiceInlineForDepartment(admin.TabularInline ):
+    model = Line
+    extra = 0
+
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc', 'company')
+    inlines = [ChoiceInlineForDepartment]
+
+class ChoiceInlineForFactory(admin.TabularInline ):
+    model = Workshop
+    extra = 0
+
+class FactoryAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc', 'company', 'city')
+    inlines = [ChoiceInlineForFactory]
+
+class ChoiceInlineForWorkshop(admin.TabularInline ):
+    model = Line
+    extra = 0
+
+class WorkshopAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc', 'factory')
+    inlines = [ChoiceInlineForWorkshop]
+
+class ChoiceInlineForLine(admin.TabularInline ):
+    model = Station
+    extra = 0
+
+class LineAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'line_type', 'desc', 'workshop', 'department')
+    inlines = [ChoiceInlineForLine]
+
+class StationAdmin(admin.ModelAdmin):
+    list_display = ('code', 'name', 'desc', 'line')
+    #inlines = [ChoiceInlineForStation]
+
+admin.site.register(Nation, NationAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Factory, FactoryAdmin)
+admin.site.register(Workshop, WorkshopAdmin)
+admin.site.register(Line, LineAdmin)
+admin.site.register(Station, StationAdmin)
+admin.site.register(Department, DepartmentAdmin)
--- factory/models.py
+++ factory/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+#import datetime
 
 # Create your models here.
 class Nation(models.Model):
     code = models.CharField(max_length=5)
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=2000)
+    desc = models.CharField(max_length=2000, null=True)
+    #created_date = models.DateTimeField('date created', default = datetime.utc())
+
+    def __str__ (self):
+        return self.name
 
     class Meta:
         db_table = u'nation'
@@ -13,7 +18,10 @@ class City(models.Model):
     nation = models.ForeignKey(Nation, on_delete=models.CASCADE, default = 0)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=2000)
+    desc = models.CharField(max_length=2000, null=True)
+
+    def __str__ (self):
+        return self.name
 
     class Meta:
         db_table = u'city'
@@ -21,18 +29,36 @@ class City(models.Model):
 class Company(models.Model):
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=2000)
+    desc = models.CharField(max_length=2000, null=True)
+
+    def __str__ (self):
+        return self.name
 
     class Meta:
         db_table = u'company'
+
+class Department(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, default = 0)
+    code = models.CharField(max_length=20)
+    name = models.CharField(max_length=200)
+    desc = models.CharField(max_length=2000, null=True)
+
+    def __str__ (self):
+        return self.name
+
+    class Meta:
+        db_table = u'department'
 
 class Factory(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE,  default = 0)
     company = models.ForeignKey(Company, on_delete=models.CASCADE,default = 0)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=2000)
+    desc = models.CharField(max_length=2000, null=True)
     address = models.CharField(max_length=2000,default='Unknown')
+
+    def __str__ (self):
+        return self.name
 
     class Meta:
         db_table = u'factory'
@@ -41,16 +67,35 @@ class Workshop(models.Model):
     factory = models.ForeignKey(Factory, on_delete=models.CASCADE, default = 0)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=2000)
+    desc = models.CharField(max_length=2000, null=True)
+
+    def __str__ (self):
+        return self.name
 
     class Meta:
         db_table = u'workshop'
 
+
 class Line(models.Model):
-    workshop = models.ForeignKey(Workshop, on_delete=models.CASCADE, default = 0)
+    LINE_TYPE = (
+        (1, 'Line'),
+        (2, 'Cell'),
+    )
+    workshop = models.ForeignKey(Workshop, 
+                                 on_delete=models.CASCADE, 
+                                 default = 0)
+    department = models.ForeignKey( Department, 
+                                    on_delete=models.CASCADE, 
+                                    default = 0)
+    line_type = models.IntegerField(
+                                    choices=LINE_TYPE,
+                                    default=1,)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=2000)
+    desc = models.CharField(max_length=2000, null=True)
+
+    def __str__ (self):
+        return self.name
 
     class Meta:
         db_table = u'line'
@@ -59,7 +104,10 @@ class Station(models.Model):
     line = models.ForeignKey(Line, on_delete=models.CASCADE, default = 0)
     code = models.CharField(max_length=20)
     name = models.CharField(max_length=200)
-    desc = models.CharField(max_length=2000)
+    desc = models.CharField(max_length=2000, null=True)
+
+    def __str__ (self):
+        return self.name
 
     class Meta:
         db_table = u'station'
