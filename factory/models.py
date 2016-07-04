7af74a648e4033d0a43663e47d1b6bd6c24792ab
saulshao <saulshao@yahoo.com>
2016-07-04 21:48:25 +0800

    move class MesCommon to mespublic app

--- factory/models.py
+++ factory/models.py
@@ -1,56 +1,10 @@
 from django.db import models
-#import datetime
+
+#import base model
+from mespublic.models import MesCommon
 
 # Create your models here.
-class MesCommon(models.Model):
-    code = models.CharField(
-                            max_length=20, 
-                            unique = True, 
-                            help_text = 'Computer readable name'
-                            )
-    short_name = models.CharField(
-                            max_length=20, 
-                            default='Unknown',
-                            help_text = 'Human readable abbr name'
-                            )                        
-    name = models.CharField(
-                            max_length=200, 
-                            help_text = 'Human readable name'
-                            )
-    desc = models.TextField(
-                            null = True,
-                            help_text = 'Long description, full name'
-                            )
-    created_time_stamp = models.DateTimeField(
-                                                auto_now_add=True,
-                                                null=True,
-                                                help_text='Create time stamp',
-                                                editable=False
-                                            ) 
-    created_by = models.CharField(
-                                    max_length=20,
-                                    default='system',
-                                    help_text='User account who creates the row',
-                                    editable=False
-                                )
-    updated_time_stamp = models.DateTimeField(
-                                                auto_now=True,
-                                                null=True,
-                                                help_text='Update time stamp',
-                                                editable=False
-                                ) 
-    updated_by = models.CharField(
-                                    max_length=20,
-                                    default='system',
-                                    help_text='User account who updates the row',
-                                    editable=False
-                                )
-    class Meta:
-        abstract = True
-        ordering = ['code','name']
-
 class Nation(MesCommon):
-    #created_date = models.DateTimeField('date created', default = datetime.utc())
 
     def __str__ (self):
         return self.name
@@ -151,7 +105,11 @@ class Line(MesCommon):
         db_table = u'line'
 
 class Station(MesCommon):
-    line = models.ForeignKey(Line, on_delete=models.PROTECT, default = 0)
+    line = models.ForeignKey(
+                              Line, 
+                              on_delete=models.PROTECT, 
+                              default = 0
+                            )
 
     def __str__ (self):
         return self.name
