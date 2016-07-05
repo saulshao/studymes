2d7626e92956469245ea77b883b0f4aafe66195c
SHAO QIANG <saulshao@yahoo.com>
2016-07-05 09:09:34 +0800

    draft for label models
    uncompleted version for label models

--- label/models.py
+++ label/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+#import base model
+from mespublic.models import MesCommon
+
+#nation or country
+class Label(MesCommon):
+
+    def __str__ (self):
+        return self.name
+
+    class Meta:
+        db_table = u'label'
+
+#City
+class LabelTemplate(MesCommon):
+    TPL_FORMAT = (
+      (1,'Barcode printer'),
+      (2,'Laser printer'),
+      )
+    
+    format_type = models.IntegerField(
+                                      choices = TPL_FORMAT
+                                      default = 1
+                                      )
+    
+    url = models.
+    
+    def __str__ (self):
+        return self.name
+
+    class Meta:
+        db_table = u'label_template'
