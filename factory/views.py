1e9dd1925b47a766b6fbcac446892b9a036f65e5
saulshao <saulshao@yahoo.com>
2016-07-17 17:56:02 +0800

    confirm the change

--- factory/views.py
+++ factory/views.py
@@ -5,9 +5,6 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response
 from .models import *
 
-def region_view(request):
-    return render_to_response('factory/templates/region.html', {'subregions':Region.objects.filter(parent = None)})
-
 def index(request):
     #return HttpResponse("Hello, world. You're at the factory index.")
     return  render_to_response('region.html', {"subregions":Region.objects.filter(parent = None)})
