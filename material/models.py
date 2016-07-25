b81d33efaf7e0bdd0fc6517d526f7b102feb394a
saulshao <saulshao@yahoo.com>
2016-07-26 07:12:29 +0800

    refactor the code architecture

--- material/models.py
+++ material/models.py
@@ -14,18 +14,31 @@ class MaterialCategory(Common, Rowtracking,Recurrence):
     class Meta:
         db_table = u'material_category'
 
-class Material(Common, Rowtracking):
-
-    material_category = models.ForeignKey(
-                                            MaterialCategory, 
-                                            on_delete=models.PROTECT, 
-                                            default = 1
-                                         )
-                                         
-    enabled = models.BooleanField(
-                                  default=True
-                                 )
+class Material(Common, Rowtracking):         
     
+    enabled = models.BooleanField(
+            default=True
+        )    
+    belong_to = models.ManyToManyField(
+        MaterialCategory,
+        through='MaterialBelongtoCategory',
+        through_fields=('material','material_category',),
+    )    
 
     class Meta:
         db_table = u'material'
+        
+class MaterialBelongtoCategory(models.Model):
+    material = models.ForeignKey(
+                                Material, 
+                                on_delete=models.PROTECT, 
+                                null = True,
+                            )
+    material_category = models.ForeignKey(
+                                    MaterialCategory, 
+                                    on_delete=models.PROTECT, 
+                                    null = True,
+                                  )
+                                  
+    class Meta:
+        db_table = u'material_category_r'
